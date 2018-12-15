3722684569af2bd4d7ddf445380caeee1b317cd4
V-O-G <cornflower1989@gmail.com>
2018-12-15 22:21:53 +0300

    move breadcrumbs & search-control to core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { BreadcrumbsComponent } from './core/breadcrumbs/breadcrumbs.component';
 import { CourseListModule } from './course-list/course-list.module';
 
 
--- src/app/course-list/course-list.module.ts
+++ src/app/course-list/course-list.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseListItemComponent } from './course-list-item/course-list-item.component';
-import { SearchControlComponent } from './search-control/search-control.component';
+import { SearchControlComponent } from '../core/search-control/search-control.component';
 
 @NgModule({
   imports: [
