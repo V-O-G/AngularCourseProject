342dafa1089e5bc4c94206765377ecee05e7f90e
V-O-G <cornflower1989@gmail.com>
2018-12-16 23:30:37 +0300

    add header elements

--- src/app/core/search-control/search-control.component.ts
+++ src/app/core/search-control/search-control.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-control',
   templateUrl: './search-control.component.html',
-  styleUrls: ['./search-control.component.css']
+  styleUrls: ['./search-control.component.scss']
 })
 export class SearchControlComponent implements OnInit {
 
