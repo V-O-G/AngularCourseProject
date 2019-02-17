56205968bd101c62e145b3afcc344c3564737cbe
V-O-G <cornflower1989@gmail.com>
2019-02-17 14:52:43 +0300

    add condition to breadcrumbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthorizationService } from './core/shared/services/authorization.service';
 
 
 @Component({
@@ -6,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  breadcrumbsActive: boolean = true;
+
+  constructor(private authorizationService: AuthorizationService) {}
+
+  ngOnInit() {
+    this.authorizationService.isUserLoggedIn.subscribe(
+      (loggedIn: boolean) => {
+        this.breadcrumbsActive = loggedIn
+      }
+    );
+  }
 }
