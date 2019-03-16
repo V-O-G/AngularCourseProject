461595e59ff11d6dd4b07c9ab12a3599b1919bfa
V-O-G <cornflower1989@gmail.com>
2019-03-16 16:53:39 +0300

    use Observable as a return value in canActivate

--- src/app/core/shared/services/auth-guard.service.ts
+++ src/app/core/shared/services/auth-guard.service.ts
@@ -7,6 +7,7 @@ import {
 import { Injectable } from '@angular/core';
 
 import { AuthorizationService } from './authorization.service';
+import { Observable, of } from 'rxjs';
 
 
 @Injectable()
@@ -17,9 +18,9 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): boolean {
+              state: RouterStateSnapshot): Observable<boolean> {
     if (this.authorizationService.isAuthenticated()) {
-      return true;
+      return of(true);
     } else {
       this.router.navigate(['/login']);
     }
